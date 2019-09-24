48c84ff581b57402a143cebcbc11985c8f940de9
spenclark <spenclark@hotmail.com>
2019-09-24 16:13:10 -0700

    request form in progress

--- landing/src/App.js
+++ landing/src/App.js
@@ -5,6 +5,10 @@ import './App.css';
 
 import LoginForm from './components/loginForm';
 import SignupForm from './components/signupForm'
+import RequestForm from './components/requestForm'
+// miniforms
+import VacationForm from './components/miniForms/vacationForm'
+
 
 function App() {
   return (
@@ -13,6 +17,8 @@ function App() {
         <Route exact path='/' component={landing}/>
         <Route path='/login' component={LoginForm} />
         <Route path='/signup' component={SignupForm} />
+        <Route path='/request' component={RequestForm}/>
+        <Route path='/vacation' component={VacationForm} />
       </Router>
     </div>
   );
