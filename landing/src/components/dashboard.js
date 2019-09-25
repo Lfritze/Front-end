3a14682f893076111bcf43a65dfe99d590ab08d8
spenclark <spenclark@hotmail.com>
2019-09-25 14:41:42 -0700

    Fixes

--- landing/src/components/dashboard.js
+++ landing/src/components/dashboard.js
@@ -2,13 +2,17 @@ import React from "react"
 import {render} from 'react-dom'
 import DashboardHeader from "./dashboardComponents/dashboardHeader"
 import CharaterList from './cardList/employeeList'
+import { NavLink } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'; 
+
 const Dashboard = () => {   
 
     return(
         <div>
         
         <DashboardHeader />
-        
+
+            <h4 className="employeeTitle">Employee List</h4>
             <div className="cardContainer">
             <CharaterList />
             </ div>
@@ -17,5 +21,5 @@ const Dashboard = () => {
 };
 
 
-render(<Dashboard />, document.getElementById('root'))
+render(<Router><Dashboard /></Router>, document.getElementById('root'))
 export default Dashboard
