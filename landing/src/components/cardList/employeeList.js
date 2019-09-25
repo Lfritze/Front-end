9db450c558b1d2eadb5f6182c892d1c83acd9281
spenclark <spenclark@hotmail.com>
2019-09-25 11:14:31 -0700

    cards

---
+++ landing/src/components/cardList/employeeList.js
@@ -0,0 +1,30 @@
+  
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import CharacterCard from './employeeCard';
+import { Route } from 'react-router-dom';
+
+export default function CharacterList() {
+  // TODO: Add useState to track data from useEffect
+  const [ chara, setChara ] = useState([])
+
+  // Useeffect / axios
+  useEffect(() => {
+   const Hook = () => {
+   axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then(e =>{
+      setChara(e.data.results)
+    })
+    }
+    Hook()
+  }, []);
+
+  return (
+    <>
+      {chara.map(data => 
+      <CharacterCard  key={data.name} data={data} />
+      )}
+    </>
+  );
+}
